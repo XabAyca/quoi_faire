d97cca97b40049b7cbe051ba5a958cda0156a1c8
Xabi <xabi.aycaguer@gmail.com>
2021-05-18 17:08:40 +0200

    ajout crea event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,12 +2,24 @@ class EventsController < ApplicationController
   before_action :authenticate_user!, except:[:index]
 
   def index
+    @events = Event.all
   end
 
   def new
+    @event = Event.new
   end
 
   def create
+    p "$"*60
+    p params
+    @event = Event.new(title:params[:title],description:params[:description],admin_id:current_user.id,duration:params[:duration],location:params[:location],price:params[:price],start_date:params[:start_date].to_datetime)
+    p @event
+    if @event.save
+      redirect_to root_path, notice: "L'évènement '#{@event.title.capitalize}' a bien été créé ! Félicitation"
+    else
+      flash.now[:messages] = @event.errors.full_messages
+      render new_event_path
+    end
   end
 
   def edit 
@@ -21,5 +33,5 @@ class EventsController < ApplicationController
 
   def destroy
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
   root 'events#index'
-  resources :events
+
+  resources  :events
   devise_for :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

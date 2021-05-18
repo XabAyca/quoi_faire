61ad101a8f36ad40b3fda5eb263e520235a12358
Xabi <xabi.aycaguer@gmail.com>
2021-05-18 13:44:15 +0200

    add devise

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,25 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, except:[:index]
+
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit 
+  end
+
+  def update 
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+  
 end

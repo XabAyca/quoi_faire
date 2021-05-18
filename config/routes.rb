058530c26a1c77c79df2b4626b981c3427e72f35
Xabi <xabi.aycaguer@gmail.com>
2021-05-18 19:38:42 +0200

    bug correction

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root 'events#index'
-  resources  :users, only: :show 
+  
   resources  :events
   devise_for :users
-
+  resources  :users, only: :show 
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

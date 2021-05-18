07221f038df2e8f3304f5dd39fde3acb9a520333
Xabi <xabi.aycaguer@gmail.com>
2021-05-18 10:58:28 +0200

    MAJ pour pascaline

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
   has_many :attendances, dependent: :destroy
   has_many :events, through: :attendances
   has_many :events, foreign_key: 'admin_id', class_name: 'Event', dependent: :destroy

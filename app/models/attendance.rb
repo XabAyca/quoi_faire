a3faae0310d6867093bf50b1b0ba1212471eb3b1
Xabi <xabi.aycaguer@gmail.com>
2021-05-19 19:01:45 +0200

    with mail

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,7 +2,7 @@ class Attendance < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
-  # after_create :attendance_created
+  after_create :attendance_created
 
   def attendance_created
     UserMailer.attendance_email(self).deliver_now
